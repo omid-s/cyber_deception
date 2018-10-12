e4c41ad209800168238b11d85a982a4ae4446328
Omid <os14526@uga.edu>
2018-10-11 22:11:31 -0400

    adds fixing of the auditd config file to script

--- behavior_graph/scripts/doAudit.bash
+++ behavior_graph/scripts/doAudit.bash
@@ -1,6 +1,23 @@
 #!/bin/bash
 
+
+#### Set up #######################
+
+configFile="/etc/audit/auditd.conf"
+
+echo "checking for audit configuration"
+if grep -q "freq = 5500" "$configFile"; then
+   echo "Configure file already ok" 
+else 
+   echo "writing configurations to audit config file"  
+   sed -i -e 's/freq = /freq = 5500\n##freq = /g'  "$configFile"
+   sed -i -e 's/flush = /flush = INCREMENTAL\n##flush = /g' "$configFile"
+   echo "done setting up config"
+fi
+
 ### Action ###############################################################
+
+
 echo "Killing auditd..."
 sudo auditctl -D
 sudo killall -9 auditd
@@ -9,6 +26,8 @@ echo "Starting auditd..."
 sudo auditd 
 
 AUDITD_PID=$(pidof auditd)
+
+echo $AUDITD_PID 
 echo "Cleaning up auditd rules..."
 sudo auditctl -D || fail 4
 
@@ -16,7 +35,7 @@ echo "Applying new set of rules to auditd..."
 
 #sudo auditctl -a exit,always -S 62 -S 60 -S 231 -F pid!=$AUDITD_PID
 
-sudo auditctl -a exit,always -F arch=b64 -S 0 -S 19 -S 1 -S 20 -S 44 -S 45 -S 46 -S 47 -S 86 -S 88 -S 56 -S 57 -S 58 -S 59 -S 2 -S 85 -S 257 -S 259 -S 133 -S 32 -S 33 -S 292 -S 49 -S 43 -S 288 -S 42 -S 82 -S 105 -S 113 -S 90 -S 22 -S 203 -S 76 -S 77 -S 40 -S 87 -S 263 -F success=1 -F pid!=$AUDITD_PID -b 1000
+sudo auditctl -a exit,always -F arch=b64 -S 0 -S 19 -S 1 -S 20 -S 44 -S 45 -S 46 -S 47 -S 86 -S 88 -S 56 -S 57 -S 58 -S 59 -S 2 -S 85 -S 257 -S 259 -S 133 -S 32 -S 33 -S 292 -S 49 -S 43 -S 288 -S 42 -S 82 -S 105 -S 113 -S 90 -S 22 -S 203 -S 76 -S 77 -S 40 -S 87 -S 263 -F success=1 -F pid!=$AUDITD_PID
 
 ##sudo auditctl -a exit,always -S all -F pid!=$AUDITD_PID
 
