b324c3f5f5aa10812bf99b13e73534c2b9c25209
Omid <os14526@uga.edu>
2018-10-16 15:36:05 -0400

    adds runtime variable infastructure

--- behavior_graph/src/ControlClasses/RuntimeVariables.java
+++ behavior_graph/src/ControlClasses/RuntimeVariables.java
@@ -3,50 +3,140 @@
  */
 package ControlClasses;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import Classes.SysdigRecordObject;
+import exceptions.VariableNoitFoundException;
+
 /**
  * @author omid
+ *
+ *         This class implements a singleton object as a reference for keeeping
+ *         runtime enviroments these values can be set using set keywords in the
+ *         query.
  *
  */
 public class RuntimeVariables {
 
-	private RuntimeVariables(){
+	private RuntimeVariables() {
 		super();
-		
+
+		// set the mappings
+		fieldsMap = new HashMap<String, Field>();
+
+		try {
+
+			fieldsMap.put("forward_depth", this.getClass().getDeclaredField("forwardDepth"));
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
 		// ****** set default values :
-		forwardDepth = -1; 
+		forwardDepth = -1;
+
 	}
-	
-	private static RuntimeVariables instance = null; 
+
+	private Map<String, Field> fieldsMap;
+
+	private static RuntimeVariables instance = null;
 
 	/**
 	 * returns the singleton instance of the runtime variable contorller
+	 * 
 	 * @return
 	 */
-	public static RuntimeVariables getInstance(){
-		if( instance ==null ){
+	public static RuntimeVariables getInstance() {
+		if (instance == null) {
 			instance = new RuntimeVariables();
 		}
 		return instance;
 	}
-	
+
 	// the depth to which a forward query will enumerate
-	private int forwardDepth;
-	
+	private Integer forwardDepth;
+
 	/**
 	 * depth to which a forward query should go
+	 * 
 	 * @return
 	 */
-	public int getForwardDepth(){
+	public int getForwardDepth() {
 		return this.forwardDepth;
 	}
-	
+
 	/**
-	 * sets depth to which a forward query should go 
+	 * sets depth to which a forward query should go
+	 * 
 	 * @param value
 	 */
-	public void setForwardDepth( int value ){
-		this.forwardDepth= value;
+	public void setForwardDepth(int value) {
+		this.forwardDepth = value;
 	}
-	
-	
+
+	/**
+	 * autoamtically sets the value for the given key
+	 * 
+	 * @param Key
+	 *            variabe to set
+	 * @param Value
+	 *            value to set the variable to
+	 * @throws VariableNoitFoundException
+	 *             thrown when the input key does not match any of the known
+	 *             varialbes
+	 */
+	public void setValue(String Key, String Value) throws VariableNoitFoundException {
+		// validate the key
+		if (!fieldsMap.containsKey(Key.toLowerCase()))
+			throw new VariableNoitFoundException(
+					String.format("Variable '%s' was not found!Please cehck and try again ... \r\n", Key));
+
+		Object TheValue = null;
+
+		switch (fieldsMap.get(Key.toLowerCase()).getType().getName()) {
+		case "java.lang.Integer":
+			TheValue = Integer.parseInt(Value);
+			break;
+		case "java.lang.String":
+			TheValue = Value;
+
+		default:
+			break;
+		}
+		try {
+			fieldsMap.get(Key.toLowerCase()).set(this, TheValue);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+
+		}
+	}
+
+	/**
+	 * gets the value for a given variable
+	 * 
+	 * @param Key
+	 *            the key to find value for
+	 * @return string representation of the value associated with that setting
+	 * @throws VariableNoitFoundException
+	 *             is thrown when the key is not known to the class
+	 */
+	public String getValue(String Key) throws VariableNoitFoundException {
+		// validate the key
+		if (!fieldsMap.containsKey(Key.toLowerCase()))
+			throw new VariableNoitFoundException(
+					String.format("Variable '%s' was not found!Please cehck and try again ... \r\n", Key));
+
+		Object TheValue = null;
+
+		try {
+			return fieldsMap.get(Key.toLowerCase()).get(this).toString();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return "Not Found!";
+		}
+	}
+
 }
