943e30d04ba0915796e9273583b591f6a1b1e550
Omid <omid.setayeshfar@msn.com>
2018-12-04 21:21:00 -0500

    adds interptere base implementation

--- behavior_graph/src/querying/ParsedQuery.java
+++ behavior_graph/src/querying/ParsedQuery.java
@@ -30,7 +30,17 @@ public class ParsedQuery {
 	private boolean doesAppend = false;
 
 	
-
+/**
+ * creates an instance of the parsed query class 
+ * @param verticeTypes type of verties to include in returned graph
+ * @param edgeTypes types of edges to inlude in the returning graph 
+ * @param iVerbose is the returning result graph supposed to be verbose? 
+ * @param isBackTracked if it's a back track query 
+ * @param isForwardTracked if it's a forward track query
+ * @param criterias the parsed criterias to be applied
+ * @param originalGraph the old graph if adding to a previous graph is desired 
+ * @param doesAppend is this a query that is supposed to be appened to a previous graph?
+ */
 	public ParsedQuery(ArrayList<ResourceType> verticeTypes, ArrayList<String> edgeTypes, boolean iVerbose,
 			boolean isBackTracked, boolean isForwardTracked, ArrayList<Criteria> criterias,
 			Graph<ResourceItem, AccessCall> originalGraph , boolean doesAppend) {
@@ -38,6 +48,19 @@ public class ParsedQuery {
 		this(verticeTypes, edgeTypes, iVerbose, isBackTracked, isForwardTracked, criterias, originalGraph, "", doesAppend);
 	}
 
+	
+	/**
+	 * creates an instance of the parsed query class 
+	 * @param verticeTypes type of verties to include in returned graph
+	 * @param edgeTypes types of edges to inlude in the returning graph 
+	 * @param iVerbose is the returning result graph supposed to be verbose? 
+	 * @param isBackTracked if it's a back track query 
+	 * @param isForwardTracked if it's a forward track query
+	 * @param criterias the parsed criterias to be applied
+	 * @param originalGraph the old graph if adding to a previous graph is desired 
+	 * @param queryString the original query as entered by the user
+	 * @param doesAppend is this a query that is supposed to be appended to a previous graph?
+	 */
 	public ParsedQuery(ArrayList<ResourceType> verticeTypes, ArrayList<String> edgeTypes, boolean iVerbose,
 			boolean isBackTracked, boolean isForwardTracked, ArrayList<Criteria> criterias,
 			Graph<ResourceItem, AccessCall> originalGraph, String queryString, boolean doesAppend) {
@@ -53,6 +76,10 @@ public class ParsedQuery {
 		this.doesAppend = doesAppend;
 	}
 
+	
+	public ParsedQuery() {
+		super();
+	}
 	
 	public boolean isDoesAppend() {
 		return doesAppend;
--- behavior_graph/src/querying/QueryInterpreter.java
+++ behavior_graph/src/querying/QueryInterpreter.java
@@ -3,13 +3,32 @@
  */
 package querying;
 
+import classes.AccessCall;
+import classes.ResourceItem;
+import edu.uci.ics.jung.graph.Graph;
+
 /**
  * @author omid
+ * 
+ *         This class will handle interpreting the entered query into a parsed
+ *         query
  *
  */
 public class QueryInterpreter {
-	
-	
-	
+
+	public static ParsedQuery interpret(String query) {
+		
+		ParsedQuery ret  = new ParsedQuery();
+		
+		return null;
+	}
+
+	public static ParsedQuery interpret(String query, Graph<ResourceItem, AccessCall> oldGraph) {
+		return null;
+	}
+
+	public static void interpret(ParsedQuery query) {
+		return;
+	}
 
 }
