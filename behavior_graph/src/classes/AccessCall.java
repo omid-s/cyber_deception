fc7cd70128973418a4d73de5eaec3c63a5760d9d
Omid <omid.setayeshfar@msn.com>
2019-01-09 18:14:49 -0500

    adds neo4j reoresentaitno to access call

--- behavior_graph/src/classes/AccessCall.java
+++ behavior_graph/src/classes/AccessCall.java
@@ -16,7 +16,7 @@ public class AccessCall extends Object {
 	public String user_name;
 	public int OccuranceFactor = 1;
 	public long sequenceNumber;
-	
+
 	public boolean isEqual(AccessCall theOther) {
 		AccessCall tmp = (AccessCall) theOther;
 		return this.From == tmp.From && this.To == tmp.To && this.Command.equals(tmp.Command);
@@ -27,6 +27,19 @@ public class AccessCall extends Object {
 		// TODO Auto-generated method stub
 		return id + " | from: " + this.From.toString() + " | to :" + this.To.toString() + " | cooamnd:" + Command
 				+ " | user:" + user_name;
+	}
+
+	/**
+	 * returns the Neo4J string representation of the object
+	 * 
+	 * @return the string representation of the access call
+	 */
+	public String toN4JObjectString() {
+		return String.format(
+				"call:%s{command:\"%s\",date:\"%s\", description:\"%s\" , "
+						+ "args:\"%s\", info:\"%s\", user_id:\"%s\" , user_name:\"%s\"  }",
+				Command, Command, DateTime, Description, args, Info, user_id, user_name);
+
 	}
 
 	@Override
