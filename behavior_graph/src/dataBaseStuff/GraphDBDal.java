d40abe04705d2c7d7a4f57868989480c7447b533
Omid <os14526@uga.edu>
2019-04-11 18:53:59 -0400

    cleans databasestuff package files

--- behavior_graph/src/dataBaseStuff/GraphDBDal.java
+++ behavior_graph/src/dataBaseStuff/GraphDBDal.java
@@ -27,6 +27,9 @@ public class GraphDBDal {
 		Class.forName("org.neo4j.jdbc.Driver").newInstance();
 	}
 
+	/**
+	 * closes the connections to the database
+	 */
 	public void closeConnections() {
 		try {
 			if (TheStateMent != null)
@@ -44,6 +47,11 @@ public class GraphDBDal {
 	private Driver driver = null;
 	ArrayList<String> Queries = new ArrayList<String>();
 
+	/**
+	 * adds the given record to be inserted to the database
+	 * @param inp the record to be inserted
+	 * @param SaveVerbose if the record is being inserted in the verbose mode
+	 */
 	public void Save(SysdigRecordObject inp, boolean SaveVerbose) {
 
 		SysdigRecordObjectGraph tempGraph = GraphObjectHelper.getGraphFromRecord(inp);
@@ -70,31 +78,17 @@ public class GraphDBDal {
 
 		temp += ";";
 
-//		try {
-//			if (TheConnection == null) {
-//				TheConnection = java.sql.DriverManager.getConnection(
-//						String.format("jdbc:neo4j:bolt://%s/",
-//								Configurations.getInstance().getSetting(Configurations.NEO4J_SERVER)),
-//						Configurations.getInstance().getSetting(Configurations.NEO4J_USERNAME),
-//						Configurations.getInstance().getSetting(Configurations.NEO4J_PASSWORD)
-//
-//				);
-//				TheStateMent = TheConnection.createStatement();
-//			}
-//
-//			TheStateMent.executeUpdate(temp);
-////			System.out.print(";");
-//		} catch (Exception ex) {
-//			ex.printStackTrace();
-//		}
-
 		Queries.add(temp);
-//
+
 		if (Queries.size() % 1000 == 0)
 			flushRows();
 
 	}
 
+	/**
+	 * flushes the rows into the database
+	 * this is done on a fixed record count interval
+	 */
 	public void flushRows() {
 
 		try {
@@ -131,8 +125,8 @@ public class GraphDBDal {
 //				);
 //				TheStateMent = TheConnection.createStatement();
 //			}
-			// Querying
-
+			
+			 
 			if (driver == null)
 				driver = GraphDatabase.driver(
 						"bolt://" + Configurations.getInstance().getSetting(Configurations.NEO4J_SERVER),
@@ -144,14 +138,11 @@ public class GraphDBDal {
 			for (String pick : Queries) {
 				trnx.run(pick);
 			}
+			
 			trnx.success();
-////			trnx.
-//
 			trnx.close();
-//			
-//			
+
 			Queries.clear();
-//			System.out.println("0");
 
 		} catch (Exception ex) {
 			ex.printStackTrace();
