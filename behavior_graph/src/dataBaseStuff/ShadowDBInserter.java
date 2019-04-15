ee9d1dc940f5d5c08555fecf6ac1cbe413f400fa
Omid <omid.setayeshfar@msn.com>
2019-04-15 16:28:20 -0400

    adds shadow inserter class

---
+++ behavior_graph/src/dataBaseStuff/ShadowDBInserter.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package dataBaseStuff;
+
+/**
+ * @author omid
+ *
+ */
+public class ShadowDBInserter {
+
+	/**
+	 * 
+	 */
+	public ShadowDBInserter() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
