ccf129bc3540343ce2364f07aa890ef3440631e7
Omid <omid.setayeshfar@msn.com>
2019-04-17 15:05:38 -0400

    adds base implementation fro shadow db inserter

--- behavior_graph/src/dataBaseStuff/ShadowDBInserter.java
+++ behavior_graph/src/dataBaseStuff/ShadowDBInserter.java
@@ -3,24 +3,43 @@
  */
 package dataBaseStuff;
 
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import classes.AccessCall;
+import classes.ResourceItem;
+
 /**
  * @author omid
  *
  */
 public class ShadowDBInserter {
 
+	private static ShadowDBInserter __Instance= null; // holds the singleton instance of shadow inserter
+	private ConcurrentLinkedQueue<ResourceItem> theResourceQue;  //
+	private ConcurrentLinkedQueue<AccessCall> theCallQue;
+	
 	/**
-	 * 
+	 * returns the singleton inserter object. 
+	 * @return the inserter object
 	 */
-	public ShadowDBInserter() {
-		// TODO Auto-generated constructor stub
+	public static ShadowDBInserter getInstance() {
+		if( __Instance == null )
+			__Instance = new ShadowDBInserter();
+		
+		return __Instance;
 	}
 	
 	
+	/**
+	 * creates an instance of the shadow inserter object 
+	 */
+	private  ShadowDBInserter() {
+		theResourceQue= new ConcurrentLinkedQueue<ResourceItem>();
+		theCallQue= new ConcurrentLinkedQueue<AccessCall>();
+		
+		//TODO : implenment the threaded inserter 
+		
+	}
 	
-	
-	
-	
-	
-
 }
