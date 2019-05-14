4d7b5ad3af65f22db614d47951e85d237146bcef
Omid <omid.setayeshfar@msn.com>
2019-05-13 20:49:21 -0400

    cleans the shadow inserter file

--- behavior_graph/src/insertion/graph/ShadowDBInserter.java
+++ behavior_graph/src/insertion/graph/ShadowDBInserter.java
@@ -27,9 +27,6 @@ import querying.tools.GraphObjectHelper;
 public class ShadowDBInserter {
 
 	private static ShadowDBInserter __Instance = null; // holds the singleton instance of shadow inserter
-	private ConcurrentLinkedQueue<ResourceItem> theResourceQue; //
-	private ConcurrentLinkedQueue<AccessCall> theCallQue;
-	private ConcurrentLinkedQueue<String> theQueryQue;
 	private ConcurrentLinkedQueue<Object> theObjectQue;
 
 	/**
@@ -48,9 +45,7 @@ public class ShadowDBInserter {
 	 * creates an instance of the shadow inserter object
 	 */
 	private ShadowDBInserter() {
-		theResourceQue = new ConcurrentLinkedQueue<ResourceItem>();
-		theCallQue = new ConcurrentLinkedQueue<AccessCall>();
-		theQueryQue = new ConcurrentLinkedQueue<String>();
+		
 		theObjectQue = new ConcurrentLinkedQueue<Object>();
 
 		Thread inserter = new Thread(new AsyncNeo4JInserter(this));
@@ -59,107 +54,17 @@ public class ShadowDBInserter {
 	}
 
 	public void insertNode(ResourceItem node) {
-		String temp = "";
 
-		temp += "\r\n" + String.format(" merge ( f:%s ) ", node.toN4JObjectString());
-
-		temp += ";";
-
-//		theQueryQue.add(temp);
 		theObjectQue.add(node);
 
-//		if (Queries.size() % 10000 == 0)
-//		{
-//			System.out.print(".");
-//			flushRows();
-////			Queries.clear();
-//		}
 	}
 
 	public void insertEdge(AccessCall edge) {
 
-//		String temp = "";
-//
-//		temp += "\r\n" + String.format(" merge ( f:%s ) ", edge.From.toN4JObjectString());
-//		temp += "\r\n" + String.format(" merge ( t:%s ) ", edge.To.toN4JObjectString());
-//		temp += "\r\n" + String.format(" merge (f)-[:%s]->(t) ", edge.toN4JObjectString());
-//
-//		temp += ";";
-
 		theObjectQue.add(edge);
 
-//	theQueryQue.add(temp);
-
-////	Queries.add(temp);
-//		if (theQueryQue.size() % 10000 == 0)
-//		{
-//			System.out.print(".");
-//			flushRows();
-////			Queries.clear();
-//		}
-//			
 	}
 
-	private static Connection TheConnection;
-	private static Statement TheStateMent;
-	private Driver driver = null;
-	ArrayList<String> Queries = new ArrayList<String>();
-
-	public void flushRows() {
-
-		try {
-
-//			try {
-//				if (TheConnection == null) {
-//					TheConnection = java.sql.DriverManager.getConnection(
-//							String.format("jdbc:neo4j:bolt://%s/",
-//									Configurations.getInstance().getSetting(Configurations.NEO4J_SERVER)),
-//							Configurations.getInstance().getSetting(Configurations.NEO4J_USERNAME),
-//							Configurations.getInstance().getSetting(Configurations.NEO4J_PASSWORD)
-//
-//					);
-//					TheStateMent = TheConnection.createStatement();
-//				}
-//				for (String temp : Queries)
-//					TheStateMent.executeUpdate(temp);
-////				System.out.print(";");
-//			} catch (Exception ex) {
-//				ex.printStackTrace();
-//			}
-
-			// Connect
-//			if (TheConnection == null) {
-//				TheConnection = DriverManager.getConnection(
-//						String.format("jdbc:neo4j:bolt://%s/",
-//								Configurations.getInstance().getSetting(Configurations.NEO4J_SERVER)),
-//						Configurations.getInstance().getSetting(Configurations.NEO4J_USERNAME),
-//						Configurations.getInstance().getSetting(Configurations.NEO4J_PASSWORD)
-//
-//				);
-//				TheStateMent = TheConnection.createStatement();
-//			}
-
-			if (driver == null)
-				driver = GraphDatabase.driver(
-						"bolt://" + Configurations.getInstance().getSetting(Configurations.NEO4J_SERVER),
-						AuthTokens.basic(Configurations.getInstance().getSetting(Configurations.NEO4J_USERNAME),
-								Configurations.getInstance().getSetting(Configurations.NEO4J_PASSWORD)));
-			Session session = driver.session();
-
-			Transaction trnx = session.beginTransaction();
-			for (String pick : Queries) {
-				trnx.run(pick);
-			}
-
-			trnx.success();
-			trnx.close();
-
-			Queries.clear();
-
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
 
 	/**
 	 * returns true if there are queris to be run
