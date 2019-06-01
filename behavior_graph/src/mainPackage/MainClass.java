a97b6e752d5f381885a195b71b1865368b557c96
Omid <omid.setayeshfar@msn.com>
2019-06-01 19:30:32 -0400

    adds memory purge to reader thread

--- behavior_graph/src/mainPackage/MainClass.java
+++ behavior_graph/src/mainPackage/MainClass.java
@@ -278,6 +278,16 @@ public class MainClass {
 //							System.out.println(counter + "(Q :" + ShadowDBInserter.getInstance().getQueLenght() + ")");
 
 							if (counter % 500000 == 0) {
+								System.out.println(String.format("\nA:remaining mem: %f - e:%d n:%d ",
+										((runtime.freeMemory() * 1.0) / runtime.totalMemory()) * 100,
+										theGraph.getEdgeCount(), theGraph.getVertexCount()));
+
+								while (runtime.freeMemory() <= runtime.totalMemory() * 0.20) {
+									System.out.print("+");
+									// memory is too full, purge some records out then flush GC
+									InMemoryAdapter.getSignleton().purge(2000, theGraph);
+									System.gc();
+								}
 
 								if (runtime.freeMemory() <= runtime.totalMemory() * 0.30) {
 									cleaner_ctr++;
@@ -291,21 +301,9 @@ public class MainClass {
 									t1.start();
 
 								}
-								if (runtime.freeMemory() <= runtime.totalMemory() * 0.10) {
-									cleaner_ctr++;
-									System.out.print("+");
-									// memory is too full, purge some records out then flush GC
-									InMemoryAdapter.getSignleton().purge(1000, theGraph);
-
-									Thread t1 = new Thread(new Runnable() {
-										@Override
-										public void run() {
-											System.gc();
-										}
-									});
-									t1.start();
-
-								}
+								System.out.println(String.format("\nB:remaining mem: %f - e:%d n:%d ",
+										((runtime.freeMemory() * 1.0) / runtime.totalMemory()) * 100,
+										theGraph.getEdgeCount(), theGraph.getVertexCount()));
 
 							}
 						}
