24f897b13845d068c5bbac8b1ce2f15af07895d1
Omid <omid.setayeshfar@msn.com>
2020-01-06 14:51:31 -0500

    adds agent transmitter

---
+++ behavior_graph/src/aiconnector/AIConnectorGraalfAgent.java
@@ -0,0 +1,15 @@
+package aiconnector;
+
+public class AIConnectorGraalfAgent implements Runnable{
+
+	public AIConnectorGraalfAgent() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void run() {
+//		TODO : implement the sending procedure
+	}
+	
+
+}
