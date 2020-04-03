8160c89a17aadafe64affb7700825c1269e34db0
Omid <omid.setayeshfar@msn.com>
2020-04-03 18:03:05 -0400

    adds observation reset to the ai connector memory

--- behavior_graph/src/aiconnector/AIConnectorMemory.java
+++ behavior_graph/src/aiconnector/AIConnectorMemory.java
@@ -9,55 +9,67 @@ import java.util.HashMap;
 /**
  * @author omid
  *
- *	This class handles the main memory for the AI connector including the variables for each query that needs to be tracked
+ *         This class handles the main memory for the AI connector including the
+ *         variables for each query that needs to be tracked
  * 
  */
 public class AIConnectorMemory {
 
-	private static AIConnectorMemory _instance = null ;
+	private static AIConnectorMemory _instance = null;
 	private HashMap<Integer, Boolean> states_map = null;
-	
-	
+
 	/***
 	 * returns a singleton object of the class
+	 * 
 	 * @return
 	 */
 	public static AIConnectorMemory getInstance() {
-		if( _instance == null) 
+		if (_instance == null)
 			_instance = new AIConnectorMemory();
-		
+
 		return _instance;
 	}
-	
+
 	/**
 	 * creates an instance of the object
 	 */
 	public AIConnectorMemory() {
-		
+
 		this.states_map = new HashMap<Integer, Boolean>();
 	}
-	
+
 	/**
 	 * sets an state for an observation.
-	 * @param observationID identifier of the observation 
-	 * @param state the state to set for the observation 
+	 * 
+	 * @param observationID identifier of the observation
+	 * @param state         the state to set for the observation
 	 */
 	public void setStateForObservation(int observationID, boolean state) {
 		this.states_map.put(observationID, state);
-		System.out.println( String.format( "%d --> %d", observationID, state? 1 : 0 ));
+		System.out.println(String.format("%d --> %d", observationID, state ? 1 : 0));
 	}
-	
+
 	/***
-	 * returns an string representation of the observations 
+	 * returns an string representation of the observations
+	 * 
 	 * @return
 	 */
 	public String getObservationString() {
 		ArrayList<String> items = new ArrayList<>();
 		for (int pick : states_map.keySet()) {
-			items.add( String.format("%d:%d", pick, states_map.get(pick)? 1 : 0) );
+			items.add(String.format("%d:%d", pick, states_map.get(pick) ? 1 : 0));
 		}
-		
-		return String.join(";",items);
+
+		return String.join(";", items);
 	}
-	
+
+	/***
+	 * sets all the observations to zero 
+	 */
+	public void resetObservations() {
+		for (int key : states_map.keySet()) {
+			states_map.put(key, false);
+		}
+	}
+
 }
